8f625ab0f77739e2cdd5a9f71e3352297b789113
kgbraden <70673111+kgbraden@users.noreply.github.com>
2020-09-09 12:51:29 -0700

    Trying again...not-committing secret_key

--- Random_Bakes/MainPage/urls.py
+++ Random_Bakes/MainPage/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from MainPage import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
 # from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
