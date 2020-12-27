5ce3678d2760f27d6beaf3c833b8a11eb014690b
kgbraden <70673111+kgbraden@users.noreply.github.com>
2020-12-27 11:23:31 -0800

    stuff

---
+++ Random_Bakes/Random_Bakes/config--.py
@@ -0,0 +1,10 @@
+jotform= "39bb440c41f05e196e9e3f53ca67b1b6"
+SSH = {
+    "Tunnel": "ssh.pythonanywhere.com",
+    "username": "kbraden",
+    "password": "N!two0dere",
+    "remote_bind_address": "kbraden.mysql.pythonanywhere-services.com",
+    "pUser": "kbraden",
+    "pPassword": "PFtqX83z6pzS7p",
+    "pDbase": "kbraden$RandomBakes",
+}
